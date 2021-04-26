89045c6d2063d01b614c37e06048640c3f3a5495
fuchunhui <fuchunhui@baidu.com>
2021-04-26 15:24:04 +0800

    feat: vite config

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+console.log('123');
+
+const na = document.createComment('a');
+document.body.appendChild(na);
+const nn = document.createElement('div');
+nn.innerHTML = '<p>测试文本内容</p>'; 
+document.body.appendChild(nn);
+
