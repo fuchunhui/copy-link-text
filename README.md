d01c9b84f9812c115d4fd492be9615a7250db48e
fuchunhui <fuchuhu7@gmail.com>
2021-05-01 18:45:25 +0800

    wip: demo1 & demo2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # copy-link-text
 about :~:text=
+
+// TODO
+关于 scroll to text fragment的blog 介绍，详见【我是链接】
+
+### 说明
+
+代码基于 web components 构建。
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-console.log('123456');
+class CopySection extends HTMLElement {
+  constructor() {
+    super()
 
-const na = document.createComment('p');
-document.body.appendChild(na);
-const div = document.createElement('div');
-div.innerHTML = '<p>测试文本内容</p>'; 
-document.body.appendChild(div);
+    const h2 = document.createElement('h2')
+    h2.textContent = this.getAttribute('t')
+    const div = document.createElement('div')
+    div.textContent = this.getAttribute('p')
+
+    const shadowRoot = this.attachShadow({mode: 'open'})
+    shadowRoot.appendChild(h2)
+    shadowRoot.appendChild(div)
+  }
+}
+
+customElements.define('copy-section', CopySection)
