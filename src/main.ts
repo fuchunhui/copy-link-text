29254a677e1024fbdab3a62d739b645a75bc1b0e
fuchunhui <fuchuhu7@gmail.com>
2021-05-03 01:28:12 +0800

    feat: adjust components logic

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,32 @@ class CopySection extends HTMLElement {
     super()
 
     const h2 = document.createElement('h2')
-    h2.textContent = this.getAttribute('t')
+    const a = document.createElement('a')
+    a.setAttribute('href', this.getAttribute('href') || '')
+    a.setAttribute('target', '_blank')
+    a.style.textDecoration = 'none'
+    a.textContent = this.getAttribute('t')
+    h2.appendChild(a)
+
     const p = document.createElement('p')
-    p.textContent = this.getAttribute('p')
+    p.textContent = this.getAttribute('n')
 
     const shadowRoot = this.attachShadow({mode: 'open'})
     shadowRoot.appendChild(h2)
     shadowRoot.appendChild(p)
+
+    const contentList = (this.textContent || '').split('-')
+    contentList.forEach((line, index) => {
+      const nl = document.createElement('span')
+      if (index === contentList.length - 1) {
+        nl.style.background = '#265ea7';
+        nl.style.color = '#ed20f9'
+      }
+      nl.textContent = line.trim()
+      const pl = document.createElement('p')
+      pl.appendChild(nl)
+      shadowRoot.appendChild(pl)
+    })
   }
 }
 
